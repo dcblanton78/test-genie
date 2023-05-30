e9babb2b44a44ab39fc36bc31e6f6fff6919a311
Clay Blanton <dcblanton78@gmail.com>
2023-05-30 11:47:59 -1000

    Adding backend files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 const App = () => {
   const [requirements, setRequirements] = useState("");
   const [testCases, setTestCases] = useState([]);
+  const API_KEY = "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";
+  console.log(API_KEY);
+  console.log("Hello!!!!!");
 
   const handleChange = (event) => {
     setRequirements(event.target.value);
@@ -26,46 +29,29 @@ const App = () => {
     event.preventDefault();
 
     const data = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content:
-            "Please provide the test cases associated with these requirements. Please include all the following information: Test Case ID, Description, and Expected Result. Provide the answer as a json object with keys for ID, Description, and Expected_Result " +
-            requirements,
-        },
-      ],
+      method: "GET",
+      url: "http://localhost:8000/test-cases",
+      params: { requirements: requirements },
     };
 
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer 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`,
-      },
-    };
-
-    try {
-      const response = await axios.post(
-        "https://api.openai.com/v1/chat/completions",
-        data,
-        config
-      );
+    axios.request(data).then(function (response) {
+      console.log("Response Data!");
+      console.log(response.data);
       const generatedTestCases = JSON.parse(
         response.data.choices[0].message.content
       ).map((testCase) => ({ ...testCase, actualResult: "", status: "New" }));
       setTestCases(generatedTestCases);
-    } catch (error) {
-      console.error(error);
-    }
+    });
   };
 
   return (
     <div>
+      <h1>TestGenie</h1>
       <form onSubmit={handleSubmit}>
-        <label>
+        <h2 className="prompt">
           Enter your software requirements:
           <textarea value={requirements} onChange={handleChange} />
-        </label>
+        </h2>
         <input type="submit" value="Generate Test Cases" />
       </form>
       {testCases.map((testCase, index) => (
@@ -114,3 +100,41 @@ const App = () => {
 };
 
 export default App;
+
+// const handleSubmit = async (event) => {
+//   event.preventDefault();
+
+//   const data = {
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       {
+//         role: "user",
+//         content:
+//           "Please provide the test cases associated with these requirements. Please include all the following information: Test Case ID, Description, and Expected Result. Provide the answer as a json object with keys for ID, Description, and Expected_Result " +
+//           requirements,
+//       },
+//     ],
+//   };
+
+//   const config = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${API_KEY}`,
+//     },
+//   };
+
+//   try {
+//     const response = await axios.post(
+//       //"https://api.openai.com/v1/chat/completions",
+//       "http://localhost:8000/test-cases",
+//       data,
+//       config
+//     );
+//     const generatedTestCases = JSON.parse(
+//       response.data.choices[0].message.content
+//     ).map((testCase) => ({ ...testCase, actualResult: "", status: "New" }));
+//     setTestCases(generatedTestCases);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
---
+++ test-genie-backend/src/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import * as dotenv from "dotenv";
+
+const PORT = 8000;
+
+const app = express();
+
+app.use(cors());
+
+app.get("/hello", (req, res) => {
+  res.json("Hello World");
+});
+
+app.get("/test-cases", async (req, res) => {
+  //const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+  const requirements = req.query.requirements;
+  console.log(requirements);
+  const API_KEY = ;
+  console.log(API_KEY);
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content:
+          "Please provide the test cases associated with these requirements. Please include all the following information: Test Case ID, Description, and Expected Result. Provide the answer as a json object with keys for ID, Description, and Expected_Result: " +
+          requirements,
+      },
+    ],
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      data,
+      config
+    );
+
+    // Send the API response as a JSON response to the client
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(8000, () => {
+  console.log("Server is listening on port 8000");
+});
